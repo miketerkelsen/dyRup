bfece85e3faba066dd349671a281cea286987da0
Mike Krogh Terkelsen <Mike@Mikes-Macbook.local>
2021-09-02 10:01:25 +0200

    Commit

--- R/colours.R
+++ R/colours.R
@@ -1,30 +1,52 @@
+#' Complete list of palettes
+#'
+#' Use \code{\link{dyRup_palette}} to construct palettes of desired length.
+#'
+#' @export
+
 dyRup_palettes <- list(
-  devoid_of_all_desires <- c('#25557B', '#F4CE80', '#C4031D'),
-  calm_down_woman <- c('#ECE4DC', '#EBD9D0', '#BCDBDB', '#C3AC98', '#B98675', '#637D74', '#607B8E'),
-  warm_modern <- c('#E5DAC8', '#ABB39E', '#A2B7B5', '#AB6057', '#C09C6A', '#604344', '#464860'),
-  brighteyes <- c('#E8EAE6', '#B6B0A9', '#414040', '#ADBAE3', '#A54975', '#4F6997', '#0190A8'),
-  dark_musk <- c('#F2EBE6', '#A0928B', '#935F43', '#3A5F7D', '#3F4C5A', '#464444', '#29545C'),
-  braveheart <- c('#EAE9E7', '#F5DEAE', '#F0B9A9', '#A49D70', '#C5394B', '#625141', '#3A5F7D'),
-  soft_n_hazy <- c('#F1EEE4', '#D3B6BA', '#B3C4BA', '#75978F', '#3A5F7D', '#645D5F', '#604046')
+  devoid_of_all_desires = c('#25557B', '#F4CE80', '#C4031D'),
+  calm_down_woman = c('#ECE4DC', '#EBD9D0', '#BCDBDB', '#C3AC98', '#B98675', '#637D74', '#607B8E'),
+  warm_modern = c('#E5DAC8', '#ABB39E', '#A2B7B5', '#AB6057', '#C09C6A', '#604344', '#464860'),
+  brighteyes = c('#E8EAE6', '#B6B0A9', '#414040', '#ADBAE3', '#A54975', '#4F6997', '#0190A8'),
+  dark_musk = c('#F2EBE6', '#A0928B', '#935F43', '#3A5F7D', '#3F4C5A', '#464444', '#29545C'),
+  braveheart = c('#EAE9E7', '#F5DEAE', '#F0B9A9', '#A49D70', '#C5394B', '#625141', '#3A5F7D'),
+  soft_n_hazy = c('#F1EEE4', '#D3B6BA', '#B3C4BA', '#75978F', '#3A5F7D', '#645D5F', '#604046')
 )
 
 #ggplot2::qplot(x = 1:7, y = 1, fill = I(rev(calm_down_woman)), geom = 'col', width = 1) + ggplot2::theme_void()
 
+#' A Wes Anderson palette generator
+#'
+#' These are a handful of color palettes from Wes Anderson movies.
+#'
+#' @param n Number of colors desired. Almost all palettes have 7 colors.
+#'   But hopefully we'll add more palettes soon.
+#'   If omitted, uses all colours.
+#' @param palette Name of desired palette. Choices are:
+#'   \code{devoid_of_all_desires}, \code{calm_down_woman},  \code{warm_modern},
+#'   \code{brighteyes}, \code{dark_musk},  \code{braveheart}, \code{soft_n_hazy}
+#' @param type Either "continuous" or "discrete". Use continuous if you want
+#'   to automatically interpolate between colours.
+#'   @importFrom graphics rgb rect par image text
+#' @return A vector of colours.
+#' @export
+
 dyRup <- function(palette, n, type = c("discrete", "continuous")) {
   type <- match.arg(type)
-  
+
   pal <- dyRup_palettes[[palette]]
   if (is.null(pal))
     stop("Palette not found.")
-  
+
   if (missing(n)) {
     n <- length(pal)
   }
-  
+
   if (type == "discrete" && n > length(pal)) {
     stop("Number of requested colors greater than what palette can offer")
   }
-  
+
   out <- switch(type,
                 continuous = grDevices::colorRampPalette(pal)(n),
                 discrete = pal[1:n]
@@ -32,16 +54,18 @@ dyRup <- function(palette, n, type = c("discrete", "continuous")) {
   structure(out, class = "palette", palette = palette)
 }
 
-
+#' @export
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
 
 print.palette <- function(x, ...) {
   n <- length(x)
   old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
   on.exit(par(old))
-  
+
   image(1:n, 1, as.matrix(1:n), col = x,
         ylab = "", xaxt = "n", yaxt = "n", bty = "n")
-  
+
   rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
   text((n + 1) / 2, 1, labels = attr(x, "palette"), cex = 1, family = "serif")
 }
